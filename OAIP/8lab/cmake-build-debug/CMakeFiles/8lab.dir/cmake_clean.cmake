4fd6a40e0169619941ccea3bb8d990dea677c0c8
Liza <liza.metla@gmail.com>
2018-12-25 14:47:53 +0300

    Net takoi laboratornoi

---
+++ OAIP/8lab/cmake-build-debug/CMakeFiles/8lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8lab.dir/main.cpp.o"
+  "8lab.pdb"
+  "8lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
