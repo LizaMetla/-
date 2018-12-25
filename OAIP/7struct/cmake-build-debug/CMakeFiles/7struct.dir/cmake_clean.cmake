2b063dfb7f55fdb93e3a1019f5b580cd0ce017fb
Liza <liza.metla@gmail.com>
2018-12-25 14:22:37 +0300

    Initial commit

---
+++ OAIP/7struct/cmake-build-debug/CMakeFiles/7struct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/7struct.dir/main.cpp.o"
+  "7struct.pdb"
+  "7struct"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7struct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
